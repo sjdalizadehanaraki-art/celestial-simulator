966da9658eb88ae232f254f1ad49946eeb6b94e3
sjdalizadehanaraki-art <sjd.alizadehanaraki@gmail.com>
2026-07-10 18:06:51 +0330

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,1 +1,68 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
+
+export function createCamera(){
+
+    const camera =
+    new THREE.PerspectiveCamera(
+        45,
+        window.innerWidth /
+        window.innerHeight,
+        0.1,
+        100
+    );
+
+
+    // نمای مورد نظر تو
+    camera.position.set(
+        -8,
+        -8,
+        8
+    );
+
+
+    camera.up.set(
+        0,
+        0,
+        1
+    );
+
+
+    camera.lookAt(
+        0,
+        0,
+        0
+    );
+
+
+
+    const controls =
+    new OrbitControls(
+        camera,
+        document.body
+    );
+
+
+    controls.enableDamping=true;
+
+    controls.enablePan=false;
+
+
+    controls.target.set(
+        0,
+        0,
+        0
+    );
+
+
+    controls.update();
+
+
+
+    return {
+        camera,
+        controls
+    };
+
+}
