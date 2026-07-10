9615aaf7f9742d7a8c90facb727b7a6d3203c33d
sjdalizadehanaraki-art <sjd.alizadehanaraki@gmail.com>
2026-07-10 18:07:17 +0330

    Update axes.js

--- axes.js
+++ axes.js
@@ -1,1 +1,48 @@
+import * as THREE from "three";
 
+
+export function createAxes(scene){
+
+
+    const length = 5;
+
+
+
+    // X قرمز
+
+    scene.add(
+        new THREE.ArrowHelper(
+            new THREE.Vector3(1,0,0),
+            new THREE.Vector3(0,0,0),
+            length,
+            0xff0000
+        )
+    );
+
+
+
+    // Y سبز
+
+    scene.add(
+        new THREE.ArrowHelper(
+            new THREE.Vector3(0,1,0),
+            new THREE.Vector3(0,0,0),
+            length,
+            0x00ff00
+        )
+    );
+
+
+
+    // Z آبی
+
+    scene.add(
+        new THREE.ArrowHelper(
+            new THREE.Vector3(0,0,1),
+            new THREE.Vector3(0,0,0),
+            length,
+            0x0088ff
+        )
+    );
+
+}
