39aa524bd952e72085b655b4aa7f33aae8dfaa7f
sjdalizadehanaraki-art <sjd.alizadehanaraki@gmail.com>
2026-07-10 21:20:27 +0330

    Update celestialSphere.js

--- celestialSphere.js
+++ celestialSphere.js
@@ -1,31 +1,42 @@
 import * as THREE from "three";
 
+
 export function createCelestialSphere(scene){
 
-    const sphere = new THREE.Mesh(
 
-        new THREE.SphereGeometry(
-            5,
-            64,
-            64
-        ),
-
-        new THREE.MeshBasicMaterial({
-
-            color:0xffffff,
-
-            transparent:true,
-
-            opacity:0.10,
-
-            side:THREE.BackSide,
-
-            depthWrite:false
-
-        })
-
+    const geometry =
+    new THREE.SphereGeometry(
+        5,
+        64,
+        64
     );
 
+
+    const material =
+    new THREE.MeshBasicMaterial({
+
+        color: 0xffffff,
+
+        transparent: true,
+
+        opacity: 0.1,
+
+        side: THREE.BackSide,
+
+        depthWrite: false
+
+    });
+
+
+
+    const sphere =
+    new THREE.Mesh(
+        geometry,
+        material
+    );
+
+
     scene.add(sphere);
+
 
 }
