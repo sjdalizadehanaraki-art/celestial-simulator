768098741d289d12bc8be993532f8be0b5a01a55
sjdalizadehanaraki-art <sjd.alizadehanaraki@gmail.com>
2026-07-10 21:57:00 +0330

    Update timeController.js

--- timeController.js
+++ timeController.js
@@ -1,1 +1,100 @@
+export function createTimeController(){
 
+
+    let running = false;
+
+
+    let speed = 1;
+
+
+    let simTime = 0;
+
+
+    let lastTime = performance.now();
+
+
+
+    function update(){
+
+        const now =
+        performance.now();
+
+
+        const realDelta =
+        (now - lastTime) / 1000;
+
+
+        lastTime = now;
+
+
+
+        if(running){
+
+            simTime +=
+            realDelta * speed;
+
+        }
+
+
+    }
+
+
+
+    function play(){
+
+        running = true;
+
+    }
+
+
+
+    function pause(){
+
+        running = false;
+
+    }
+
+
+
+    function setSpeed(value){
+
+        speed = value;
+
+    }
+
+
+
+    function getTime(){
+
+        return simTime;
+
+    }
+
+
+
+    function reset(){
+
+        simTime = 0;
+
+    }
+
+
+
+    return {
+
+        update,
+
+        play,
+
+        pause,
+
+        setSpeed,
+
+        getTime,
+
+        reset
+
+    };
+
+
+}
