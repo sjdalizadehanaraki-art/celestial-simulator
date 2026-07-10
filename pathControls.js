4483b53a50118bb0a83fc316eea594f673f5b2ab
sjdalizadehanaraki-art <sjd.alizadehanaraki@gmail.com>
2026-07-10 22:21:49 +0330

    Update pathControls.js

--- pathControls.js
+++ pathControls.js
@@ -1,1 +1,46 @@
+export function createPathControls(trail){
 
+
+    const button =
+    document.createElement("button");
+
+
+    button.textContent =
+    "☑ Show Sun Path";
+
+
+    button.style.marginTop =
+    "10px";
+
+
+    button.onclick = ()=>{
+
+
+        trail.toggle();
+
+
+        if(trail.isVisible()){
+
+            button.textContent =
+            "☑ Show Sun Path";
+
+        }
+
+        else{
+
+            button.textContent =
+            "☐ Show Sun Path";
+
+        }
+
+
+    };
+
+
+
+    document.body.appendChild(
+        button
+    );
+
+
+}
