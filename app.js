31ae7034afb99cde0a92e50988d9ced273eee94f
sjdalizadehanaraki-art <sjd.alizadehanaraki@gmail.com>
2026-07-11 01:06:17 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,191 +1,146 @@
-import { createTimeController } from "./timeController.js";
-import { createSunMotion } from "./sunApparentMotion.js";
-import { createSun } from "./sun.js";
+import { createSeasonPoints } from "./seasonPoints.js";
 import { createSunTrail } from "./sunTrail.js";
+import { createTimeControls } from "./timeControls.js";
+import { createTimeController } from "./timeController.js";
+import { createSun } from "./sun.js";
+import { createSunMotion } from "./sunApparentMotion.js";
+import { createCelestialPlanes } from "./celestialPlanes.js";
+
 import * as THREE from "three";
 import { CSS2DRenderer } from "three/addons/renderers/CSS2DRenderer.js";
+
 import { createCamera } from "./camera.js";
 import { createAxes } from "./axes.js";
 import { createEarth } from "./earth.js";
 import { createEarthEquator } from "./earthEquator.js";
 import { createCelestialSphere } from "./celestialSphere.js";
-import { createCelestialPlanes } from "./celestialPlanes.js";
 
+export function createApp() {
 
-export function createApp(){
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x000000);
 
+    const time = createTimeController();
 
-    const scene =
-    new THREE.Scene();
+    const { camera, controls } = createCamera();
 
-
-    const time =
-createTimeController();
-
-console.log("TIME CREATED");
-
-
-    scene.background =
-    new THREE.Color(0x000000);
-
-
-
-    const {camera, controls} =
-    createCamera();
-
-
-
-    const renderer =
-    new THREE.WebGLRenderer({
-
-        antialias:true
-
+    const renderer = new THREE.WebGLRenderer({
+        antialias: true
     });
-
-
 
     renderer.setSize(
         window.innerWidth,
         window.innerHeight
     );
 
-
     renderer.setPixelRatio(
         window.devicePixelRatio
     );
-
 
     document.body.appendChild(
         renderer.domElement
     );
 
-
-
-    const labelRenderer =
-    new CSS2DRenderer();
-
+    const labelRenderer = new CSS2DRenderer();
 
     labelRenderer.setSize(
         window.innerWidth,
         window.innerHeight
     );
 
-
-    labelRenderer.domElement.style.position =
-    "absolute";
-
-
-    labelRenderer.domElement.style.top =
-    "0";
-
-
-    labelRenderer.domElement.style.left =
-    "0";
-
-
-    labelRenderer.domElement.style.pointerEvents =
-    "none";
-
+    labelRenderer.domElement.style.position = "absolute";
+    labelRenderer.domElement.style.top = "0";
+    labelRenderer.domElement.style.left = "0";
+    labelRenderer.domElement.style.pointerEvents = "none";
 
     document.body.appendChild(
         labelRenderer.domElement
     );
 
-
-
-
     scene.add(
-
         new THREE.AmbientLight(
             0xffffff,
             0.8
         )
-
     );
 
-
-
     createAxes(scene);
-
     createEarth(scene);
-
     createEarthEquator(scene);
-
     createCelestialSphere(scene);
-
     createCelestialPlanes(scene);
+    createSeasonPoints(scene);
 
-    const sunTrail =
-createSunTrail();
+    const sun = createSun(scene);
 
+    const sunMotion = createSunMotion(
+        sun,
+        time
+    );
 
-scene.add(
-    sunTrail.line
-);
+    const sunTrail = createSunTrail();
 
-    const sun =
-createSun(scene);
+    scene.add(
+        sunTrail.line
+    );
 
+    createTimeControls(
+        time,
+        sunTrail
+    );
 
-    function animate(){
-
+    function animate() {
 
         requestAnimationFrame(
             animate
         );
 
-        controls.update();
+        time.update();
 
+        sunMotion.update();
+
+        sunTrail.addPoint(
+            sun.position,
+            time.getTime()
+        );
+
+        controls.update();
 
         renderer.render(
             scene,
             camera
         );
 
-
         labelRenderer.render(
             scene,
             camera
         );
 
-
     }
-
-
 
     animate();
 
-
-
-
     window.addEventListener(
         "resize",
-        ()=>{
-
+        () => {
 
             camera.aspect =
-            window.innerWidth /
-            window.innerHeight;
-
+                window.innerWidth /
+                window.innerHeight;
 
             camera.updateProjectionMatrix();
-
-
 
             renderer.setSize(
                 window.innerWidth,
                 window.innerHeight
             );
 
-
             labelRenderer.setSize(
                 window.innerWidth,
                 window.innerHeight
             );
 
-
         }
     );
-
 
 }
