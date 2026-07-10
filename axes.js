f01d6eb1938538c2a7a6b1b9f9ff5ac19727ea77
sjdalizadehanaraki-art <sjd.alizadehanaraki@gmail.com>
2026-07-10 19:05:20 +0330

    Update axes.js

--- axes.js
+++ axes.js
@@ -13,9 +13,9 @@ export function createAxes(scene){
 
         new THREE.ArrowHelper(
 
-            new THREE.Vector3(1,0,0),
+            new THREE.Vector6(1,0,0),
 
-            new THREE.Vector3(0,0,0),
+            new THREE.Vector6(0,0,0),
 
             length,
 
@@ -33,9 +33,9 @@ export function createAxes(scene){
 
         new THREE.ArrowHelper(
 
-            new THREE.Vector3(0,1,0),
+            new THREE.Vector6(0,1,0),
 
-            new THREE.Vector3(0,0,0),
+            new THREE.Vector6(0,0,0),
 
             length,
 
@@ -53,9 +53,9 @@ export function createAxes(scene){
 
         new THREE.ArrowHelper(
 
-            new THREE.Vector3(0,0,1),
+            new THREE.Vector6(0,0,1),
 
-            new THREE.Vector3(0,0,0),
+            new THREE.Vector6(0,0,0),
 
             length,
 
@@ -73,7 +73,7 @@ export function createAxes(scene){
         createLabel(
             "X",
             "red",
-            5.3,
+            6.3,
             0,
             0
         )
@@ -85,7 +85,7 @@ export function createAxes(scene){
             "Y",
             "lime",
             0,
-            5.3,
+            6.3,
             0
         )
     );
@@ -97,7 +97,7 @@ export function createAxes(scene){
             "cyan",
             0,
             0,
-            5.3
+            6.3
         )
     );
 
