da4584c214f90820b7ad037a7c422abf25a56be5
sjdalizadehanaraki-art <sjd.alizadehanaraki@gmail.com>
2026-07-11 00:46:27 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,139 +1,174 @@
 import * as THREE from "three";
 
-alert("THREE OK");
-
 import { CSS2DRenderer } from "three/addons/renderers/CSS2DRenderer.js";
 
-alert("CSS2D OK");
+import { createCamera } from "./camera.js";
+import { createAxes } from "./axes.js";
+import { createEarth } from "./earth.js";
+import { createEarthEquator } from "./earthEquator.js";
+import { createCelestialSphere } from "./celestialSphere.js";
+import { createCelestialPlanes } from "./celestialPlanes.js";
+
+
 export function createApp(){
 
-    const scene = new THREE.Scene();
 
-    scene.background = new THREE.Color(0x000000);
-
-    const time =
-    createTimeController();
-
-const {camera, controls} =
-createCamera();
+    const scene =
+    new THREE.Scene();
 
 
-const renderer = new THREE.WebGLRenderer({
-    antialias:true
-});
+    scene.background =
+    new THREE.Color(0x000000);
 
 
-renderer.setSize(
-    window.innerWidth,
-    window.innerHeight
-);
+
+    const {camera, controls} =
+    createCamera();
 
 
-renderer.setPixelRatio(
-    window.devicePixelRatio
-);
+
+    const renderer =
+    new THREE.WebGLRenderer({
+
+        antialias:true
+
+    });
 
 
-document.body.appendChild(
-    renderer.domElement
-);
 
-    const labelRenderer = new CSS2DRenderer();
+    renderer.setSize(
+        window.innerWidth,
+        window.innerHeight
+    );
+
+
+    renderer.setPixelRatio(
+        window.devicePixelRatio
+    );
+
+
+    document.body.appendChild(
+        renderer.domElement
+    );
+
+
+
+    const labelRenderer =
+    new CSS2DRenderer();
+
 
     labelRenderer.setSize(
         window.innerWidth,
         window.innerHeight
     );
 
-    labelRenderer.domElement.style.position="absolute";
-    labelRenderer.domElement.style.top="0";
-    labelRenderer.domElement.style.left="0";
-    labelRenderer.domElement.style.pointerEvents="none";
+
+    labelRenderer.domElement.style.position =
+    "absolute";
+
+
+    labelRenderer.domElement.style.top =
+    "0";
+
+
+    labelRenderer.domElement.style.left =
+    "0";
+
+
+    labelRenderer.domElement.style.pointerEvents =
+    "none";
+
 
     document.body.appendChild(
         labelRenderer.domElement
     );
 
+
+
+
     scene.add(
+
         new THREE.AmbientLight(
             0xffffff,
             0.8
         )
+
     );
 
+
+
     createAxes(scene);
+
     createEarth(scene);
+
     createEarthEquator(scene);
+
     createCelestialSphere(scene);
+
     createCelestialPlanes(scene);
-  //  createSeasonPoints(scene);
-    //const sun =
-   // createSun(scene);
-   // const sunMotion =
-   // createSunMotion(sun,time);
-    const sunTrail =
-createSunTrail();
-
-
-scene.add(
-    sunTrail.line
-);
-
-
-//createTimeControls(
-//    time,
- //   sunTrail
-//);
 
 
 
     function animate(){
 
-        requestAnimationFrame(animate);
 
-        time.update();
+        requestAnimationFrame(
+            animate
+        );
 
-      //  sunMotion.update();
-
-     //   sunTrail.addPoint(
-   // sun.position,
-  //  0
-//);
 
         controls.update();
+
 
         renderer.render(
             scene,
             camera
         );
 
+
         labelRenderer.render(
             scene,
             camera
         );
 
+
     }
+
+
 
     animate();
 
-    window.addEventListener("resize",()=>{
 
-        camera.aspect =
-        window.innerWidth /
-        window.innerHeight;
 
-        camera.updateProjectionMatrix();
 
-        renderer.setSize(
-            window.innerWidth,
-            window.innerHeight
-        );
+    window.addEventListener(
+        "resize",
+        ()=>{
 
-        labelRenderer.setSize(
-            window.innerWidth,
-            window.innerHeight
-        );
 
-    });
+            camera.aspect =
+            window.innerWidth /
+            window.innerHeight;
+
+
+            camera.updateProjectionMatrix();
+
+
+
+            renderer.setSize(
+                window.innerWidth,
+                window.innerHeight
+            );
+
+
+            labelRenderer.setSize(
+                window.innerWidth,
+                window.innerHeight
+            );
+
+
+        }
+    );
+
 
 }
