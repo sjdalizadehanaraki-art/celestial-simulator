870e01de68144c396bd340c552044c67b59b4f75
sjdalizadehanaraki-art <sjd.alizadehanaraki@gmail.com>
2026-07-10 22:13:44 +0330

    Update sunTrail.js

--- sunTrail.js
+++ sunTrail.js
@@ -1,1 +1,86 @@
+import * as THREE from "three";
 
+
+export function createSunTrail(){
+
+
+    const points = [];
+
+
+    const geometry =
+    new THREE.BufferGeometry();
+
+
+
+    const material =
+    new THREE.LineBasicMaterial({
+
+        color:0xffff00,
+
+        transparent:true,
+
+        opacity:0.8
+
+    });
+
+
+
+    const line =
+    new THREE.Line(
+        geometry,
+        material
+    );
+
+
+    line.visible = true;
+
+
+    return {
+
+
+        line,
+
+
+        addPoint(position){
+
+
+            points.push(
+                position.clone()
+            );
+
+
+            geometry.setFromPoints(
+                points
+            );
+
+
+        },
+
+
+        show(){
+
+            line.visible = true;
+
+        },
+
+
+        hide(){
+
+            line.visible = false;
+
+        },
+
+
+        clear(){
+
+            points.length = 0;
+
+            geometry.setFromPoints(
+                points
+            );
+
+        }
+
+    };
+
+}
