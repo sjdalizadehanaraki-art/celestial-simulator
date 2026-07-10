72a63e3a1ab59a56bb0575cd1392d6cd2c9114ae
sjdalizadehanaraki-art <sjd.alizadehanaraki@gmail.com>
2026-07-10 20:33:37 +0330

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,9 +1,6 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-export function createCamera(){
-
-    alert("CAMERA 1");
+export function createCamera() {
 
     const camera = new THREE.PerspectiveCamera(
         45,
@@ -12,35 +9,12 @@ export function createCamera(){
         100
     );
 
-    alert("CAMERA 2");
-
     camera.position.set(15, 15, 15);
 
     camera.up.set(0, 0, 1);
 
     camera.lookAt(0, 0, 0);
 
-    alert("CAMERA 3");
-
-    const controls = new OrbitControls(
-        camera,
-        document.body
-    );
-
-    alert("CAMERA 4");
-
-    controls.enableDamping = true;
-    controls.enablePan = false;
-
-    controls.target.set(0, 0, 0);
-
-    controls.update();
-
-    alert("CAMERA 5");
-
-    return {
-        camera,
-        controls
-    };
+    return camera;
 
 }
