102f0b044d72020ba0a29b6c01d788e7e3375a44
sjdalizadehanaraki-art <sjd.alizadehanaraki@gmail.com>
2026-07-10 18:07:54 +0330

    Update earth.js

--- earth.js
+++ earth.js
@@ -1,1 +1,89 @@
+import * as THREE from "three";
 
+
+export function createEarth(scene){
+
+
+    const earth =
+    new THREE.Mesh(
+
+        new THREE.SphereGeometry(
+            1,
+            64,
+            64
+        ),
+
+
+        new THREE.MeshBasicMaterial({
+            color:0x777777,
+            transparent:true,
+            opacity:0.8
+        })
+
+    );
+
+
+    scene.add(earth);
+
+
+
+    // نقاط مرجع
+
+
+    // +X اعتدال بهاری
+    createPoint(
+        scene,
+        new THREE.Vector3(1.2,0,0),
+        0xff0000
+    );
+
+
+    // +Y طول 90 شرقی
+    createPoint(
+        scene,
+        new THREE.Vector3(0,1.2,0),
+        0x00ff00
+    );
+
+
+    // +Z قطب شمال
+    createPoint(
+        scene,
+        new THREE.Vector3(0,0,1.2),
+        0x0088ff
+    );
+
+
+}
+
+
+
+function createPoint(
+    scene,
+    position,
+    color
+){
+
+    const point =
+    new THREE.Mesh(
+
+        new THREE.SphereGeometry(
+            0.08,
+            16,
+            16
+        ),
+
+
+        new THREE.MeshBasicMaterial({
+            color:color
+        })
+
+    );
+
+
+    point.position.copy(position);
+
+
+    scene.add(point);
+
+}
