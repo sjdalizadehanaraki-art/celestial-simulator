fd1b9d156f52b8fb002d8d15777667cc76114c4f
sjdalizadehanaraki-art <sjd.alizadehanaraki@gmail.com>
2026-07-10 19:00:47 +0330

    Update earthEquator.js

--- earthEquator.js
+++ earthEquator.js
@@ -1,1 +1,24 @@
+import * as THREE from "three";
 
+export function createEarthEquator(scene){
+
+    const equator = new THREE.Mesh(
+
+        new THREE.TorusGeometry(
+            1,
+            0.01,
+            16,
+            128
+        ),
+
+        new THREE.MeshBasicMaterial({
+
+            color:0x00ff00
+
+        })
+
+    );
+
+    scene.add(equator);
+
+}
