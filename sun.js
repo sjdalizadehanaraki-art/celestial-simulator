9c12ab233dc618980885cbb4cce14a02121fef02
sjdalizadehanaraki-art <sjd.alizadehanaraki@gmail.com>
2026-07-10 21:45:46 +0330

    Update sun.js

--- sun.js
+++ sun.js
@@ -1,1 +1,39 @@
+import * as THREE from "three";
 
+
+export function createSun(scene){
+
+
+    const sun =
+    new THREE.Mesh(
+
+        new THREE.SphereGeometry(
+            0.12,
+            32,
+            32
+        ),
+
+        new THREE.MeshBasicMaterial({
+
+            color:0xffff00
+
+        })
+
+    );
+
+
+    // شروع اعتدال بهاری روی +X
+
+    sun.position.set(
+        5,
+        0,
+        0
+    );
+
+
+    scene.add(sun);
+
+
+    return sun;
+
+}
