31902eaa847f104d2c7ddf435477142ffdf04477
sjdalizadehanaraki-art <sjd.alizadehanaraki@gmail.com>
2026-07-11 15:07:49 +0330

    Update timeController.js

--- timeController.js
+++ timeController.js
@@ -1,57 +1,44 @@
 export function createTimeController(){
 
-
     const DAY_SECONDS = 5;
     const YEAR_DAYS = 365;
 
-
     let playing = true;
-
     let speed = 1;
-
 
     let lastRealTime =
     performance.now();
 
-
-
+    // روز سال
     let dayOfYear = 0;
 
+    // شروع از اعتدال بهاری
+    // ساعت 18:16
 
-    // شروع از ساعت 18:16 اعتدال بهاری
     let timeOfDay =
-    DAY_SECONDS * (18.266 / 24);
+    DAY_SECONDS *
+    (18.2666666667 / 24);
+
 
 
 
     function update(){
 
-
         const now =
         performance.now();
-
-
 
         const delta =
         (now - lastRealTime) / 1000;
 
-
         lastRealTime = now;
 
-
-
-        if(!playing) return;
-
-
+        if(!playing)
+            return;
 
         timeOfDay +=
         delta * speed;
 
-
-
-        while(
-            timeOfDay >= DAY_SECONDS
-        ){
+        while(timeOfDay >= DAY_SECONDS){
 
             timeOfDay -= DAY_SECONDS;
 
@@ -59,14 +46,24 @@ export function createTimeController(){
 
         }
 
+        while(timeOfDay < 0){
 
+            timeOfDay += DAY_SECONDS;
 
-        dayOfYear =
-        dayOfYear % YEAR_DAYS;
+            dayOfYear--;
 
+        }
 
+        while(dayOfYear >= YEAR_DAYS)
+            dayOfYear -= YEAR_DAYS;
+
+        while(dayOfYear < 0)
+            dayOfYear += YEAR_DAYS;
 
     }
+
+
+
 
 
 
@@ -104,10 +101,8 @@ export function createTimeController(){
 
     function getDayFraction(){
 
-        return (
-            timeOfDay /
-            DAY_SECONDS
-        );
+        return timeOfDay /
+        DAY_SECONDS;
 
     }
 
@@ -118,16 +113,41 @@ export function createTimeController(){
         return (
             dayOfYear +
             getDayFraction()
-        )
-        /
-        YEAR_DAYS;
+        ) / YEAR_DAYS;
 
     }
 
 
 
-    return {
 
+
+    // ساعت خورشیدی
+
+    function getSolarHour(){
+
+        return
+        getDayFraction() * 24;
+
+    }
+
+
+
+
+
+    // زاویه چرخش کره سماوی
+
+    function getSiderealAngle(){
+
+        return
+        getDayFraction() * 360;
+
+    }
+
+
+
+
+
+    return{
 
         update,
 
@@ -141,10 +161,12 @@ export function createTimeController(){
 
         getDayFraction,
 
-        getYearFraction
+        getYearFraction,
 
+        getSolarHour,
+
+        getSiderealAngle
 
     };
-
 
 }
