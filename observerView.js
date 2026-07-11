fe0586764f5e07f8e5cfd3de9aad1eb4e1c5f19c
sjdalizadehanaraki-art <sjd.alizadehanaraki@gmail.com>
2026-07-11 19:51:15 +0330

    Update observerView.js

--- observerView.js
+++ observerView.js
@@ -12,43 +12,71 @@ export function createObserverView(
 
 
 
-    scene.add(group);
-
-
-
-
-
-    // کره سماوی ناظر
-
-    const sphere =
-    new THREE.Mesh(
-
-        new THREE.SphereGeometry(
-            5,
-            64,
-            64
-        ),
-
-        new THREE.MeshBasicMaterial({
-
-            color:0xffffff,
-
-            transparent:true,
-
-            opacity:0.05,
-
-            side:THREE.BackSide,
-
-            depthWrite:false
-
-        })
-
+    scene.add(
+        group
     );
+
+
+
+
+
+
+
+    // نیم کره آسمان
+
+    const geometry =
+    new THREE.SphereGeometry(
+        5,
+        64,
+        32,
+        0,
+        Math.PI * 2,
+        0,
+        Math.PI / 2
+    );
+
+
+
+    const material =
+    new THREE.MeshBasicMaterial({
+
+        color:0x004400,
+
+        transparent:true,
+
+        opacity:0.15,
+
+        side:THREE.BackSide,
+
+        depthWrite:false
+
+    });
+
+
+
+    const sky =
+    new THREE.Mesh(
+        geometry,
+        material
+    );
+
+
+
+    // چون دوربین داخل کره است
+    // نیمه بالا باید بالای سر باشد
+
+    sky.rotation.x =
+    0;
+
 
 
     group.add(
-        sphere
+        sky
     );
+
+
+
+
 
 
 
