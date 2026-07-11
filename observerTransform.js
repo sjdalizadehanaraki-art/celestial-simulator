3c5c7263df5e82417695315dc9a28851b66080fb
sjdalizadehanaraki-art <sjd.alizadehanaraki@gmail.com>
2026-07-11 13:15:00 +0330

    Update observerTransform.js

--- observerTransform.js
+++ observerTransform.js
@@ -13,31 +13,135 @@ export function createObserverTransform(
 
 
 
-    function update(){
+
+    function equatorialToHorizontal(
+        rightAscension,
+        declination,
+        hourAngle
+    ){
 
 
-        const latitude =
-        observer.getLatitude();
 
-
-
-        const latRad =
+        const lat =
         THREE.MathUtils.degToRad(
-            latitude
+            observer.latitude
         );
 
 
 
-        /*
-        
-        تبدیل دستگاه سماوی به دستگاه ناظر
-        
-        فعلاً فقط شیب ناشی از عرض جغرافیایی
-        
-        */
 
-        group.rotation.x =
-        -(Math.PI/2 - latRad);
+        const dec =
+        THREE.MathUtils.degToRad(
+            declination
+        );
+
+
+
+
+        const H =
+        THREE.MathUtils.degToRad(
+            hourAngle
+        );
+
+
+
+
+
+        // ارتفاع
+
+        const sinAlt =
+        Math.sin(lat) *
+        Math.sin(dec)
+        +
+        Math.cos(lat) *
+        Math.cos(dec) *
+        Math.cos(H);
+
+
+
+        const altitude =
+        Math.asin(
+            sinAlt
+        );
+
+
+
+
+
+
+        // آزیموت
+
+        const y =
+        Math.sin(H);
+
+
+
+        const x =
+        Math.cos(H) *
+        Math.sin(lat)
+        -
+        Math.tan(dec) *
+        Math.cos(lat);
+
+
+
+
+        let azimuth =
+        Math.atan2(
+            y,
+            x
+        );
+
+
+
+        azimuth += Math.PI;
+
+
+
+
+
+
+        const radius = 5;
+
+
+
+
+
+        const position =
+        new THREE.Vector3();
+
+
+
+
+
+        position.x =
+        radius *
+        Math.cos(altitude) *
+        Math.sin(azimuth);
+
+
+
+
+
+        position.y =
+        radius *
+        Math.sin(altitude);
+
+
+
+
+
+        position.z =
+        radius *
+        Math.cos(altitude) *
+        Math.cos(azimuth);
+
+
+
+
+
+
+        return position;
 
 
 
@@ -46,16 +150,30 @@ export function createObserverTransform(
 
 
 
-    update();
+
+
+
+    function update(){
+
+
+
+    }
+
+
+
 
 
 
 
     return {
 
+
         group,
 
-        update
+        update,
+
+        equatorialToHorizontal
+
 
     };
 
