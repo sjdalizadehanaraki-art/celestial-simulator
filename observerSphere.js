b154d56220a9a6ddf3799338c16b4daed3dff37f
sjdalizadehanaraki-art <sjd.alizadehanaraki@gmail.com>
2026-07-11 20:40:45 +0330

    Create observerSphere.js

---
+++ observerSphere.js
@@ -0,0 +1,74 @@
+import * as THREE from "three";
+
+
+export function createObserverSphere(){
+
+
+    const group =
+    new THREE.Group();
+
+
+
+
+
+    const geometry =
+    new THREE.SphereGeometry(
+        10,
+        64,
+        64
+    );
+
+
+
+
+
+    const material =
+    new THREE.MeshBasicMaterial({
+
+        color:0xffffff,
+
+        transparent:true,
+
+        opacity:0.08,
+
+        side:THREE.BackSide,
+
+        depthWrite:false
+
+    });
+
+
+
+
+
+    const sphere =
+    new THREE.Mesh(
+
+        geometry,
+
+        material
+
+    );
+
+
+
+
+
+    group.add(
+        sphere
+    );
+
+
+
+
+
+    return {
+
+        group,
+
+        sphere
+
+    };
+
+
+}
